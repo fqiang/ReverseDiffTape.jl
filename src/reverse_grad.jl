e1bd70b2555cdf24ac2748f11b5bd8aa2e9304cd
Feng Qiang <f.qiang@gmail.com>
2015-11-11 11:09:40 -0600

    reverse pass gradient

--- src/reverse_grad.jl
+++ src/reverse_grad.jl
@@ -1,135 +1,123 @@
 
 # forward pass on the tape tt, to build ss stack
-function reverse_grad_0(tt::TT_TYPE, idx::IDX_TYPE,ss::TV_STACK,vvals::TV_TYPE,pvals::TV_TYPE)
-	# debug("enter - ",idx)
-	assert(idx != 0)
-	val = NaN
-	ntype = tt[idx] #type node
-	idx -= 1
-	if(ntype == TYPE_P)
-		val = pvals[tt[idx]]
-		idx -=1
-		# push!(ss, val)
-	elseif(ntype == TYPE_V)
-		vidx = tt[idx]
-		idx -= 1
-		val = vvals[vidx]
-		# push!(ss,val)
-	elseif(ntype == TYPE_OB)
-		oc = tt[idx]
-		assert(B_OP_START<= oc <= B_OP_END)
-		idx -= 1
-		ridx = tt[idx]
-		idx -= 1
-		lidx = tt[idx]
-		idx -= 1
-		# debug("before right - ",ridx)
-		rval = reverse_grad_0(tt,ridx,ss,vvals, pvals)
-		# debug("before left - ",lidx)
-		lval = reverse_grad_0(tt,lidx,ss,vvals, pvals)
-		(val,ld,rd) = reverse_grad_calc(oc,lval,rval)
-		# push!(ss,val)
-		push!(ss,rd)
-		push!(ss,ld)
-	elseif(ntype == TYPE_OU)
-		oc = tt[idx]
-		assert(U_OP_START <= oc <= U_OP_END)
-		idx -= 1
-		lidx = tt[idx]
-		idx -= 1
-		# debug("unary before  - ",lidx)
-		lval = reverse_grad_0(tt,lidx,ss,vvals, pvals)
-		(val,ld) = reverse_grad_calc(oc,lval)
-		# push!(ss,val)
-		push!(ss,ld)
-	else
-		assert(false)
+
+#forward pass on the scalar function
+function forward_pass{V,I}(tt::Array{I,1}, vvals::Array{V,1}, pvals::Array{V,1}, imm::Array{V,1})
+	idx = one(I)
+	v = [zero(V)]
+	stk = Array{V,1}() #used for value evaluation
+	empty!(imm)  #used for immediate derrivative
+	sizehint!(imm,round(I,length(tt)/3.5))
+	sizehint!(stk,round(I,length(tt)/10))
+	sizehint!(v,1)
+
+	@inbounds while(idx <= length(tt))
+		# @show idx
+		println("++++++++++++++++++++++++++++++++++++")
+		ntype = tt[idx]
+		idx += 1
+		if(ntype == TYPE_P)
+			# tic()
+			push!(stk,pvals[tt[idx]])	
+			idx += 2 #skip TYPE_P
+		elseif(ntype == TYPE_V)
+			push!(stk,vvals[tt[idx]])
+			idx += 2 #skip TYPE_V
+		elseif(ntype == TYPE_O)
+			oc = tt[idx]
+			idx += 1
+			n = tt[idx]
+			idx += 1
+			idx += 1 #skip TYPE_O
+			# @show OP[oc],stk
+			@show OP[oc], length(stk)-n+1, n
+			eval_idd(OP[oc],stk,length(stk)-n+1,imm,v)
+			@show imm
+			@show v
+			resize!(stk,length(stk)-n+1)
+			stk[end] = v[1]
+		end
+		println("++++++++++++++++++++++++++++++++++++")
 	end
-	assert(!isnan(val))
-	# debug("exit - ",idx)
-	return val
+	@show stk
 end
 
-function reverse_grad_calc(oc::OP_TYPE,lval::VV_TYPE, rval::VV_TYPE)
-	ld = NaN
-	rd = NaN
-	# println(OP[oc],",",lval,",",rval)
-	val = evaluate(OP[oc],lval,rval)
-	# println("reverse_grad_calc  ",val)
-	if(OP[oc]==:+)
-		ld = 1.0
-		rd = 1.0
-	elseif(OP[oc]==:-)
-		ld = 1.0
-		rd = -1.0
-	elseif(OP[oc]==:*)
-		ld = rval
-		rd = lval
-	elseif(OP[oc]==:/)
-		ld = 1.0/rval
-		rd = -lval/rval^2
-	elseif(OP[oc]==:^)
-		ld = rval*(lval^(rval-1))
-		rd = val*(log(lval))
-	else
-		assert(false)
+
+function eval_idd{V,I}(s::Symbol,v::Array{V,1},i::I,imm::Array{V,1},r::Array{V,1})
+	if(s==:sin)
+		ld = cos(v[i])
+		push!(imm,ld)
+		r[1] = sin(v[i])
+	elseif(s==:cos)
+		ld = -sin(v[i])
+		push!(imm,ld)
+		r[1] = cos(v[i])
+	elseif(s ==:+)
+		ret = zero(V)
+		@simd for j=i:1:length(v)
+			push!(imm,one(V))
+			ret += v[j]
+		end
+		r[1] = ret
+	elseif(s ==:-)
+		push!(imm,one(V))
+		push!(imm,-one(V))
+		r[1] = (-)(v[i],v[i+1])
+	elseif(s==:*)
+		# for i=length(v)-n+1:1:length(v)
+			# ret = one(V)
+			# for j=length(v)-n+1:1:i
+			# 	ret *= v[j]
+			# end
+			# for j=i+1:1:length(v)
+			# 	ret *= v[j]
+			# end
+		ret = one(V)
+		@simd for j = i:1:length(v)
+			ret *= v[j]
+		end
+		r[1] = ret
+		@simd for j = i:1:length(v)
+			push!(imm,r[1]/v[j])
+		end
+	elseif(s==:/)
+		r[1] = (/)(v[i],v[i+1])
+		push!(imm,one(V)/v[i+1])
+		push!(imm,-v[i]/v[i+1]^2)
+		
+	elseif(s==:^)
+		r[1] = (^)(v[i],v[i+1])
+		push!(imm,v[i+1]*(v[i]^(v[i+1]-1)))
+		push!(imm,r[1]*(log(v[i])))
 	end
-	assert(ld!=NaN && rd!=NaN)
-	return (val,ld,rd)
 end
 
-function reverse_grad_calc(oc::OP_TYPE,lval::VV_TYPE)
-	ld = NaN
-	val = evaluate(OP[oc],lval)
-	if(OP[oc]==:sin)
-		ld = cos(lval)
-	elseif(OP[oc]==:cos)
-		ld = -sin(lval)
-	else
-		assert(false)
-	end
-	assert(ld != NaN)
-	return (val,ld)
-end
 
-function reverse_grad_1(tt::TT_TYPE, idx::IDX_TYPE, ss::TV_STACK, adj::VV_TYPE, vvals::TV_TYPE, pvals::TV_TYPE, grad::Array{Tuple{IDX_TYPE,VV_TYPE},1})
-	# assert(length(vvals) == length(grad))
-	# debug("enter - ", adj)
-	ntype = tt[idx]
-	idx -= 1
-	if(ntype ==TYPE_P)  
-		#do nothing , this is a parameter
-	elseif(ntype == TYPE_V)
-		vidx = tt[idx]
+function reverse_pass{V,I}(tt::Array{I,1},imm::Array{V,1},g::Array{Tuple{I,V},1})
+	idx = length(tt)
+	adjs = Array{V,1}()
+	sizehint!(adjs, round(I,length(tt)/10))  #random guessing size
+	push!(adjs,one(V))  #init value 
+
+	while(idx > 0)
+		ntype = tt[idx]
 		idx -= 1
-		# debug("vidx ",vidx, "  ",adj)
-		push!(grad,(vidx,adj))
-		# grad[vidx] += adj
-	elseif(ntype == TYPE_OB)
-		dl = pop!(ss)
-		dr = pop!(ss)
-		idx -= 1 #skip oc
-		ridx = tt[idx]
-		idx -= 1
-		lidx = tt[idx]
-		idx -= 1
-		l_adj = adj*dl
-		r_adj = adj*dr
-		# debug("before left ",lidx, " ", adj," ", dl," ", l_adj)
-		reverse_grad_1(tt,lidx,ss,l_adj,vvals,pvals,grad)
-		# debug("before right ",ridx," ", adj," ", dr," ", r_adj)
-		reverse_grad_1(tt,ridx,ss,r_adj,vvals,pvals,grad)
-	elseif(ntype == TYPE_OU)
-		dl = pop!(ss)
-		idx -= 1 #skip oc
-		lidx = tt[idx]
-		idx -= 1
-		l_adj = adj*dl
-		reverse_grad_1(tt,lidx,ss,l_adj,vvals,pvals,grad)
-	else
-		assert(false)
+		adj = pop!(adjs)
+		# val = pop!(v)
+		if(ntype == TYPE_P)
+			idx -= 2
+		elseif(ntype == TYPE_V)
+			push!(g,(tt[idx],adj))
+			idx -= 2
+		elseif(ntype == TYPE_O)
+			n = tt[idx]
+			idx -= 3 #skip TYPE_O 
+			@simd for i=length(imm)-n+1:1:length(imm)
+				push!(adjs,imm[i]*adj)
+			end
+			resize!(imm,length(imm)-n)
+		end
 	end
-	# debug("exit")
 end
 
 function grad_nz(tt::TT_TYPE, idx::IDX_TYPE, I::Set{IDX_TYPE}) #non-repeat version
