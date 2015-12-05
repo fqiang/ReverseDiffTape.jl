6f6762961da0118a26f48dbd2ca5ecf69f73ebff
Feng Qiang <f.qiang@gmail.com>
2015-12-05 02:17:42 -0600

    update opf testing case

--- interface/opf.jl
+++ interface/opf.jl
@@ -1,61 +1,5 @@
-# model from 
-#  https://github.com/mlubin/JuMPSupplement/blob/master/acpower/opf.jl
-#
-
-include("interface.jl") #tape interface now
-
-function report(m)
-    eval_f = 0.0
-    eval_g = 0.0
-    eval_grad_f = 0.0
-    eval_jac_g = 0.0
-    eval_hesslag = 0.0
-
-    jeval_f2 = 0.0
-    jeval_g2 = 0.0
-    jeval_grad_f2 = 0.0
-    jeval_jac_g2 = 0.0
-    jeval_hesslag2 = 0.0
-
-    for i=1:10
-        solve(m)
-        eval_f += m.internalModel.evaluator.eval_f_timer
-        eval_g += m.internalModel.evaluator.eval_g_timer
-        eval_grad_f += m.internalModel.evaluator.eval_grad_f_timer
-        eval_jac_g += m.internalModel.evaluator.eval_jac_g_timer
-        eval_hesslag += m.internalModel.evaluator.eval_hesslag_timer
-       
-        jeval_f2 += m.internalModel.evaluator.jeval_f_timer
-        jeval_g2 += m.internalModel.evaluator.jeval_g_timer
-        jeval_grad_f2 += m.internalModel.evaluator.jeval_grad_f_timer
-        jeval_jac_g2 += m.internalModel.evaluator.jeval_jac_g_timer
-        jeval_hesslag2 += m.internalModel.evaluator.jeval_hesslag_timer
-    end
-    ratio_f2 = eval_f/jeval_f2
-    ratio_g2 = eval_g/jeval_g2
-    ratio_grad_f2 = eval_grad_f/jeval_grad_f2
-    ratio_jac_g2 = eval_jac_g/jeval_jac_g2
-    ratio_hesslag2 = eval_hesslag/jeval_hesslag2
-    return ratio_f2, ratio_g2, ratio_grad_f2, ratio_jac_g2, ratio_hesslag2
-end
-
-function run_test_suite(n,m)
-    TIMES = n
-    matrix = Array{Any,2}(5,TIMES)
-    matrix[1,1] = "eval_f" 
-    matrix[2,1] = "eval_g" 
-    matrix[3,1] = "eval_grad_f" 
-    matrix[4,1] = "eval_jac_g"
-    matrix[5,1] = "eval_hesslag"  
-
-    for i=2:1:TIMES
-        ret = report(m)
-        for j in 1:length(ret)
-            matrix[j,i] = ret[j]
-        end
-    end
-end
-
+include("/Users/fqiang/.julia/v0.4/ReverseDiffTape/interface/interface.jl")
+using TapeInterface
 
 using JuMP
 using Ipopt
@@ -102,7 +46,7 @@ function opf(bus, branch,
     branch_tap_min, branch_tap_max,
     p_gen_upper, p_gen_lower)
 
-    mod = Model(solver=IpoptSolver(max_iter=3))
+    mod = Model(solver=TapeSolver(IpoptSolver(max_iter=3)))
 
     nbus = length(bus)
     nbranch = length(branch)
@@ -228,8 +172,58 @@ function opf(bus, branch,
     println("solving")
     solve(mod)
 
-    run_test_suite(5,mod)
 
+    TIMES = 6
+    matrix = Array{Any,2}(5,TIMES+1)
+    matrix[1,1] = "eval_f" 
+    matrix[2,1] = "eval_g" 
+    matrix[3,1] = "eval_grad_f" 
+    matrix[4,1] = "eval_jac_g"
+    matrix[5,1] = "eval_hesslag"  
+
+    for i=1:TIMES
+        @time ret = report(mod,10)
+        for j in 1:length(ret)
+            matrix[j,i+1] = ret[j]
+        end
+    end
+    @show matrix
+end
+
+
+function report(mod,N)
+    eval_f = 0.0
+    eval_g = 0.0
+    eval_grad_f = 0.0
+    eval_jac_g = 0.0
+    eval_hesslag = 0.0
+
+    jeval_f2 = 0.0
+    jeval_g2 = 0.0
+    jeval_grad_f2 = 0.0
+    jeval_jac_g2 = 0.0
+    jeval_hesslag2 = 0.0
+
+    for i=1:N
+        solve(mod)
+        eval_f += mod.internalModel.evaluator.eval_f_timer
+        eval_g += mod.internalModel.evaluator.eval_g_timer
+        eval_grad_f += mod.internalModel.evaluator.eval_grad_f_timer
+        eval_jac_g += mod.internalModel.evaluator.eval_jac_g_timer
+        eval_hesslag += mod.internalModel.evaluator.eval_hesslag_timer
+       
+        jeval_f2 += mod.internalModel.evaluator.jeval_f_timer
+        jeval_g2 += mod.internalModel.evaluator.jeval_g_timer
+        jeval_grad_f2 += mod.internalModel.evaluator.jeval_grad_f_timer
+        jeval_jac_g2 += mod.internalModel.evaluator.jeval_jac_g_timer
+        jeval_hesslag2 += mod.internalModel.evaluator.jeval_hesslag_timer
+    end
+    ratio_f2 = eval_f/jeval_f2
+    ratio_g2 = eval_g/jeval_g2
+    ratio_grad_f2 = eval_grad_f/jeval_grad_f2
+    ratio_jac_g2 = eval_jac_g/jeval_jac_g2
+    ratio_hesslag2 = eval_hesslag/jeval_hesslag2
+    return ratio_f2, ratio_g2, ratio_grad_f2, ratio_jac_g2, ratio_hesslag2
 end
 
 function prepdata(bus, branch)
@@ -286,7 +280,7 @@ function prepdata(bus, branch)
     return branches, buses
 end
 
-numbuses = 662
+numbuses = ARGS[1]
 branches, buses = prepdata(readdlm("IEEE$numbuses.bus"),readdlm("IEEE$numbuses.branch"))
 
 
